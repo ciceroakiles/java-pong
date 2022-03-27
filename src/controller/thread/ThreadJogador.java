1a40b813b33196b0c728f2b32ebdeca150fa543a
ciceroakiles <cicero.akiles@gmail.com>
2022-03-27 18:31:31 -0300

    Parte 5 - checarReset, placar e mov. jogador

--- src/controller/thread/ThreadJogador.java
+++ src/controller/thread/ThreadJogador.java
@@ -1,17 +1,33 @@
 package controller.thread;
 
 import controller.util.ControleTempo;
+import model.Desenho;
+import view.Janela;
 
 public class ThreadJogador extends Thread {
-
-	public ThreadJogador() {
-		
+	
+	private Desenho rect;
+	private boolean isCPU;
+	private int step;
+	
+	public ThreadJogador(Desenho d) {
+		this.rect = d;
+		this.isCPU = true;
 	}
 	
 	public void run() {
 		while (true) {
 			ControleTempo.esperar();
-			
+			if (this.isCPU) {
+				moverCPU();
+			}
+		}
+	}
+
+	private void moverCPU() {
+		step = Janela.getBola().getY() - (int)(rect.getHeight()/2);
+		if (step > 0 && step < Janela.getHeight()-rect.getHeight()) {
+			rect.setY(step);
 		}
 	}
 }
--- src/controller/thread/ThreadJogo.java
+++ src/controller/thread/ThreadJogo.java
@@ -5,17 +5,49 @@ import view.Janela;
 
 public class ThreadJogo extends Thread {
 
+	private int pontosP1 = 0;
+	private int pontosP2 = 0;
 	private ThreadBola tb;
 	
 	public ThreadJogo() {
 		tb = new ThreadBola(Janela.getBola());
 		tb.start();
+		ThreadJogador tp1 = new ThreadJogador(Janela.getRectP1());
+		tp1.start();
+		ThreadJogador tp2 = new ThreadJogador(Janela.getRectP2());
+		tp2.start();
 	}
 	
 	public void run() {
 		while (true) {
 			ControleTempo.esperar();
 			
+			checarReset();
 		}
+	}
+
+	private void checarReset() {
+		if (escapouEsq() || escapouDir()) {
+			atualizaPlacar();
+			Janela.getBola().setX((int)(Janela.getWidth()-Janela.getBola().getWidth())/2);
+			Janela.getBola().setY((int)(Janela.getHeight()-Janela.getBola().getHeight())/2);
+			Janela.getRectP1().setY((int)(Janela.getHeight()-Janela.getRectP1().getHeight())/2);
+			Janela.getRectP2().setY((int)(Janela.getHeight()-Janela.getRectP2().getHeight())/2);
+			tb.randomDir();
+		}
+	}
+	
+	private void atualizaPlacar() {
+		if (escapouEsq()) pontosP2++;
+		if (escapouDir()) pontosP1++;
+		System.out.println(pontosP1 + " x " + pontosP2);
+	}
+
+	private boolean escapouEsq() {
+		return (Janela.getBola().getX()+Janela.getBola().getWidth() <= 0);
+	}
+	
+	private boolean escapouDir() {
+		return (Janela.getBola().getX()-Janela.getBola().getWidth() >= Janela.getWidth());
 	}
 }
